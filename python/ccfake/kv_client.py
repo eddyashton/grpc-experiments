6aa854b28f6f3e390b22fdd512b5dda591064722
Eddy Ashton <edashton@microsoft.com>
2022-05-13 15:03:14 +0000

    Rename and split

--- python/ccfake/kv_client.py
+++ python/ccfake/kv_client.py
@@ -1,40 +1,41 @@
 import grpc
 
-import ccfake_pb2
-import ccfake_pb2_grpc
+
+import kv_pb2
+import kv_pb2_grpc
 
 
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = ccfake_pb2_grpc.KVStub(channel)
+        stub = kv_pb2_grpc.KVStub(channel)
 
         table = "foo"
         key = b"\x42"
 
         print("----")
-        response = stub.Get(ccfake_pb2.GetRequest(table=table, key=key))
+        response = stub.Get(kv_pb2.GetRequest(table=table, key=key))
         print(response)
         print(response.HasField("value"))
         print(response.value)
 
         print("----")
-        response = stub.Put(ccfake_pb2.PutRequest(table=table, key=key, value=b"0x00"))
+        response = stub.Put(kv_pb2.PutRequest(table=table, key=key, value=b"0x00"))
         print(response)
         print(response.existed)
 
         print("----")
-        response = stub.Get(ccfake_pb2.GetRequest(table=table, key=key))
+        response = stub.Get(kv_pb2.GetRequest(table=table, key=key))
         print(response)
         print(response.HasField("value"))
         print(response.value)
 
         print("----")
-        response = stub.Put(ccfake_pb2.PutRequest(table=table, key=key, value=b"0x01"))
+        response = stub.Put(kv_pb2.PutRequest(table=table, key=key, value=b"0x01"))
         print(response)
         print(response.existed)
 
         print("----")
-        response = stub.Get(ccfake_pb2.GetRequest(table=table, key=key))
+        response = stub.Get(kv_pb2.GetRequest(table=table, key=key))
         print(response)
         print(response.HasField("value"))
         print(response.value)
--- python/ccfake/kv_server.py
+++ python/ccfake/kv_server.py
@@ -3,17 +3,22 @@ from collections import defaultdict
 
 import grpc
 
-import ccfake_pb2
-import ccfake_pb2_grpc
+import registry_pb2
+import registry_pb2_grpc
 
+import kv_pb2
+import kv_pb2_grpc
 
-class KV(ccfake_pb2_grpc.KVServicer):
+class Registry(registry_pb2_grpc.RegistryServicer):
+    pass
+
+class KV(kv_pb2_grpc.KVServicer):
     def __init__(self):
         self.kv = defaultdict(dict)
         self.available_clients = []
 
     def Get(self, request, context):
-        ret = ccfake_pb2.GetResponse()
+        ret = kv_pb2.GetResponse()
         table = self.kv[request.table]
         if request.key in table:
             ret.value = table[request.key]
@@ -23,12 +28,12 @@ class KV(ccfake_pb2_grpc.KVServicer):
         table = self.kv[request.table]
         exists = request.key in table
         table[request.key] = request.value
-        return ccfake_pb2.PutResponse(existed=exists)
+        return kv_pb2.PutResponse(existed=exists)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    ccfake_pb2_grpc.add_KVServicer_to_server(KV(), server)
+    kv_pb2_grpc.add_KVServicer_to_server(KV(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
