ddcd984d276474178aa1e2a6cd31e38a5822495e
Eddy Ashton <edashton@microsoft.com>
2022-05-13 16:15:36 +0000

    Progress?

--- python/ccfake/kv_server.py
+++ python/ccfake/kv_server.py
@@ -1,5 +1,7 @@
-from concurrent import futures
 from collections import defaultdict
+from concurrent import futures
+from enum import Enum
+from threading import Thread
 
 import grpc
 
@@ -9,8 +11,42 @@ import registry_pb2_grpc
 import kv_pb2
 import kv_pb2_grpc
 
+DEFAULT_DISPATCH_CATEGORIES = {"/foo", "/bar"}
+
+
+class ExecutorState(Enum):
+    Registered = 0
+    Ready = 1
+    Executing = 2
+
+
+class Executor:
+    def __init__(self):
+        self.state = ExecutorState.Registered
+
+
 class Registry(registry_pb2_grpc.RegistryServicer):
-    pass
+    def __init__(self, server, categories=DEFAULT_DISPATCH_CATEGORIES):
+        self.server = server
+        self.allowed_categories = categories
+        self.executors = {}
+
+    def Register(self, request, context):
+        ret = registry_pb2.RegisterResponse()
+        if request.dispatch_category not in self.allowed_categories:
+            ret.accepted = False
+            allowed_s = "\n".join(self.allowed_categories)
+            ret.error = f"'{request.dispatch_category}' is not a known dispatch category.\nAllowed categories are:\n{allowed_s}"
+        elif request.executor_ident in self.executors:
+            ret.accepted = False
+            ret.error = f"Executor with this identity is already registered. Identity is:\n{request.executor_ident}"
+        else:
+            self.executors[request.executor_ident] = Executor()
+            ret.accepted = True
+            # TODO: Modify server to allow connections from this ident?
+
+        return ret
+
 
 class KV(kv_pb2_grpc.KVServicer):
     def __init__(self):
@@ -31,12 +67,52 @@ class KV(kv_pb2_grpc.KVServicer):
         return kv_pb2.PutResponse(existed=exists)
 
 
+def encode_accepted_client_certs(registry):
+    certs = b"\n".join(k for k in registry.executors.keys())
+    return certs
+
+
+def fetch_server_cert_config(server_ident, registry):
+    def fn():
+        server_credentials = grpc.ssl_server_certificate_configuration(
+            server_ident, encode_accepted_client_certs(registry)
+        )
+        return server_credentials
+
+    return fn
+
+
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    kv_pb2_grpc.add_KVServicer_to_server(KV(), server)
-    server.add_insecure_port("[::]:50051")
-    server.start()
-    server.wait_for_termination()
+    registration_server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    registry = Registry(registration_server)
+    registry_pb2_grpc.add_RegistryServicer_to_server(registry, registration_server)
+    # TODO: This should be mutually auth'd
+    registration_server.add_insecure_port("[::]:50051")
+
+    with open("key.pem", "rb") as f:
+        private_key = f.read()
+    with open("cert.pem", "rb") as f:
+        certificate_chain = f.read()
+
+    server_ident = ((private_key, certificate_chain),)
+
+    kv_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    kv = KV()
+    kv_pb2_grpc.add_KVServicer_to_server(kv, kv_server)
+    dynamic_creds = grpc.dynamic_ssl_server_credentials(
+        grpc.ssl_server_certificate_configuration(server_ident),
+        fetch_server_cert_config(server_ident, registry),
+        True,
+    )
+    kv_server.add_secure_port("[::]:50052", dynamic_creds)
+
+    kv_thread = Thread(target=lambda: kv_server.start())
+    kv_thread.start()
+
+    registration_server.start()
+    registration_server.wait_for_termination()
+
+    kv_server.stop()
 
 
 if __name__ == "__main__":
--- python/ccfake/registry_pb2.py
+++ python/ccfake/registry_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0eregistry.proto\x12\x06\x63\x63\x66\x61ke\")\n\x0fRegisterRequest\x12\x16\n\x0e\x65xecutor_ident\x18\x01 \x01(\x0c\"$\n\x10RegisterResponse\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\x32K\n\x08Registry\x12?\n\x08Register\x12\x17.ccfake.RegisterRequest\x1a\x18.ccfake.RegisterResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0eregistry.proto\x12\x06\x63\x63\x66\x61ke\"D\n\x0fRegisterRequest\x12\x16\n\x0e\x65xecutor_ident\x18\x01 \x01(\x0c\x12\x19\n\x11\x64ispatch_category\x18\x02 \x01(\t\"3\n\x10RegisterResponse\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\x12\r\n\x05\x65rror\x18\x02 \x01(\t2K\n\x08Registry\x12?\n\x08Register\x12\x17.ccfake.RegisterRequest\x1a\x18.ccfake.RegisterResponse\"\x00\x62\x06proto3')
 
 
 
@@ -39,9 +39,9 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _REGISTERREQUEST._serialized_start=26
-  _REGISTERREQUEST._serialized_end=67
-  _REGISTERRESPONSE._serialized_start=69
-  _REGISTERRESPONSE._serialized_end=105
-  _REGISTRY._serialized_start=107
-  _REGISTRY._serialized_end=182
+  _REGISTERREQUEST._serialized_end=94
+  _REGISTERRESPONSE._serialized_start=96
+  _REGISTERRESPONSE._serialized_end=147
+  _REGISTRY._serialized_start=149
+  _REGISTRY._serialized_end=224
 # @@protoc_insertion_point(module_scope)
